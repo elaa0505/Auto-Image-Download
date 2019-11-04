af3d804d3fbd3de6af4c96479110dfd3b4ca9fc6
Elango SK <elaa0505@users.noreply.github.com>
2019-11-04 15:37:42 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+'''
+Author : Elango SK
+Searching and Downloading Google Images
+'''
+
+from __future__ import absolute_import
+
+
+def main():
+    import google_images_download.google_images_download
+
+if __name__ == '__main__':
+    main()
---
+++ __main__.py
@@ -0,0 +1,11 @@
+'''
+Author : Elango SK
+Searching and Downloading Google Images
+'''
+
+from __future__ import absolute_import
+
+from .__init__ import main
+
+if __name__ == '__main__':
+    main()
